e3ae79a38ef90e3450d1630eca9ef22718d13b4a
broyerdamien <broyerdamien@gmail.com>
2022-11-28 16:01:39 +0100

    quete4

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -1,5 +1,5 @@
 <?php
-// src/Controller/ProgramController.php
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ProgramController extends AbstractController
 {
-    #[Route('/program/', name: 'program_index')]
+    #[Route('/program', name: 'app_program')]
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
-            'website' => 'Wild Series',
+            'website' => 'Wild series',
         ]);
     }
 }
