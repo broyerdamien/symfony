532ded71692ada1463d8dfc4cca74bb3ec574abe
broyerdamien <broyerdamien@gmail.com>
2022-11-30 11:49:12 +0100

    quete7

--- migrations/Version20221130095638.php
+++ migrations/Version20221130095638.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20221129150331 extends AbstractMigration
+final class Version20221130095638 extends AbstractMigration
 {
     public function getDescription(): string
     {
